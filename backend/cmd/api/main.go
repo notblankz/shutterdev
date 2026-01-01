fee6aed2018a41da0206530cdaf0a49c3276006a
notblankz <aahan.clg@gmail.com>
2026-01-01 13:23:32 +0530

    Chore(backend): configure CORS to allow frontend requests

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -22,6 +24,14 @@ func main() {
 
 	fmt.Println("Starting server...")
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("NEXT_APP_PUBLIC_URL")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.Static("/public", "./public")
 	r.StaticFile("/", "./public/index.html")
