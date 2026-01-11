a31d921b0453573e4b0d3992387d676e29ac4700
notblankz <aahan.clg@gmail.com>
2026-01-11 23:42:17 +0530

    chore(frontend): add shadcn/ui skeleton component

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-90cd5a22295545318a35bad0caa1d160.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
