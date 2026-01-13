97a55579269352249942bff63884c103c4cf1365
notblankz <aahan.clg@gmail.com>
2026-01-13 22:15:38 +0530

    refactor(/test-gallery): use MaterialUI <Masonry/> component for the masonry grid

--- frontend/src/app/test-gallery/page.js
+++ frontend/src/app/test-gallery/page.js
@@ -4,7 +4,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { Skeleton } from "@/components/ui/skeleton";
+import Masonry from '@mui/lab/Masonry';
 
 const LIMIT = 20
 const shimmer = (w, h) => `
@@ -22,34 +22,10 @@ const shimmer = (w, h) => `
 </svg>`;
 
 const toBase64 = (str) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+    typeof window === "undefined"
+        ? Buffer.from(str).toString("base64")
+        : window.btoa(str);
 
-function ImageCard({index, src, width, height}) {
-    const [loaded, setLoaded] = useState(false)
-
-    return (
-        <div key={index} className="mb-4 break-inside-avoid">
-            {!loaded && <Skeleton className={`rounded-md h-${height}px w-${width}px`}/>}
-            {/* check performance difference between preload and priority loading */}
-            <Image
-                src={src}
-                width={width}
-                height={height}
-                alt="photo by photographer" // TODO: add description as alt text
-                className="rounded-md"
-                // priority={index < 6}
-                placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(width, height))}`}
-                onLoadingComplete={() => setLoaded(true)}
-                preload={true}
-                unoptimized // TODO: check perf diff between unoptimised and optimised
-            />
-        </div>
-    )
-}
-
-// TODO: Convert each image into it's own Component so that we can try to implement skeletons
 export default function TestGalleryPage() {
 
     const [offset, setOffset] = useState(0);
@@ -72,7 +48,7 @@ export default function TestGalleryPage() {
                 const photos = await res.json()
                 setPhotos(photos)
                 return photos
-            } catch(e) {
+            } catch (e) {
                 console.log(e)
             }
         }
@@ -83,11 +59,20 @@ export default function TestGalleryPage() {
     return (
         // TOOD: implement pagination
         <div className="flex justify-center items-center px-40 py-10">
-            <div className="columns-2 md:columns-3 gap-4">
+            <Masonry columns={3} spacing={2}>
                 {photos.map((photo, index) => (
-                    <ImageCard key={photo.id} index={index} src={photo.thumbnailUrl} width={photo.thumbWidth} height={photo.thumbHeight}/>
+                    <Image
+                        src={photo.thumbnailUrl}
+                        width={photo.thumbWidth}
+                        height={photo.thumbHeight}
+                        alt="photo by photographer" // TODO: add description as alt text
+                        className="rounded-md"
+                        placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(photo.thumbWidth, photo.thumbHeight))}`}
+                        preload={true}
+                        unoptimized // TODO: check perf diff between unoptimised and optimised
+                    />
                 ))}
-            </div>
+            </Masonry>
         </div>
 
     )
