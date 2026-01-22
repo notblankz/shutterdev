92e40fd93ba2181dfeed15ad8088808a5748ae60
notblankz <aahan.clg@gmail.com>
2026-01-22 20:12:41 +0530

    fix: add galleryStore.js

---
+++ frontend/src/galleryStore.js
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+
+export const usePhotosStore = create((set, get) => ({
+    photos: [],
+    cursor: null,
+    hasMore: true,
+    loading: false,
+
+    appendPhotos: (photos) => {
+        set((state) => ({
+            photos: [...state.photos, ...photos]
+        }))
+    },
+
+    resetPhotos: () => set({photos: [], cursor: null, hasMore: true, loading: false}),
+
+    getIndexById: (id) => {
+        return get().photos.findIndex((p) => p.id === id)
+    },
+
+    fetchNextPage: async () => {
+
+        const {cursor, hasMore, loading} = get()
+        if (!hasMore || loading) return
+
+        set({loading: true})
+
+        try {
+            const requestLink =
+                process.env.NEXT_PUBLIC_API_URL + (cursor ? `/api/photos?cursor=${btoa(JSON.stringify(cursor))}` : "/api/photos")
+
+            const res = await fetch(requestLink, {method: "GET"})
+            if (!res.ok) {
+                const err = await res.json()
+                console.log(err)
+                throw new Error("Failed to fetch photos");
+            }
+            const page = await res.json()
+            set((state) => ({
+                photos: (cursor ? [...state.photos, ...page.photos] : [...page.photos]),
+                cursor: page.nextCursor,
+                hasMore: page.hasMore,
+                loading: false
+            }))
+        } catch (e) {
+            console.error(e)
+            set({loading: false})
+        }
+    }
+}))
